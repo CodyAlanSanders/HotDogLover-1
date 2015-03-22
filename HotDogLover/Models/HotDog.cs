2a68f4081cbcfbdb845245cdfcc4bbeda8539fd2
Wesley Reisz <wes@wesleyreisz.com>
2015-03-22 13:15:02 -0400

    added models for the application

---
+++ HotDogLover/Models/HotDog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotDogLover.Models
+{
+    public class HotDog
+    {
+        public string HotDogName { get; set; }
+        public DateTime LastTimeAte { get; set; }
+        public string LastPlaceAte { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
---
+++ HotDogLover/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotDogLover.Models
+{
+    public class Profile
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Picture { get; set; }
+        public HotDog FavoriteHotDog { get; set; }
+        public List<HotDog> HotDogList { get; set; }
+    }
+}
