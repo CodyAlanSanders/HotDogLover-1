214be1e4b68c49354184a89711baf34b88614bc0
Wesley Reisz <wes@wesleyreisz.com>
2015-03-31 19:46:49 -0400

    added lamba samples

---
+++ HotDogLover.Tests/lambdas/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace HotDogLover.Tests.lambdas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] numbers = new int[] { 10,4,16,7,8,3,6,8,45,2};
+            var list = numbers.Take(6).OrderBy(m => m);
+           
+            Assert.IsNotNull(list);
+
+        }
+    }
+}
